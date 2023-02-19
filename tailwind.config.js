6fa6d5f7523d5e885b6c4bb22522d4e39ca1a19a
Ricardo Lara <rlara@zellship.com>
2023-02-18 21:37:24 -0600

    add tailwind darkmode config & react useDarkMode hook

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,11 @@ module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@headlessui/tailwindcss')
+  ],
 }
